8964b37a27a6fd20333216d9538d62c2c5dca414
HP Dietz <hanspeter.dietz@check24.de>
2018-01-25 16:29:46 +0100

    Update goto sha.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.3
+* Update goto sha.
+
 ## 1.0.2
 * Update goto version.
 
--- Formula/goto.rb
+++ Formula/goto.rb
@@ -2,7 +2,7 @@ class Goto < Formula
   desc "Bash completion for fast project navigation."
   homepage "https://haensl.github.io/goto"
   url "https://github.com/haensl/goto/archive/v2.1.0.tar.gz"
-  sha256 "a16e159b8f6d4ac22ba919fb09e7e80e463277b5c0185836df9113022ea0c56d"
+  sha256 "6e858bda33b115a5b0de3825345c19ecc8fe301f1c038266c70636afb65f5434"
   depends_on "bash-completion"
 
   def install
