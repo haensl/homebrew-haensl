b7d0cfa1abc455e57e21fc0a2411e3c9b7023aaf
HP Dietz <hanspeter.dietz@check24.de>
2018-01-26 15:41:01 +0100

    WIP: debugging.

--- Formula/goto.rb
+++ Formula/goto.rb
@@ -6,13 +6,14 @@ class Goto < Formula
   sha256 "8d23f9ee132d77de80f36f9260453a8e169d3c5706f0ecf3f8b3337123bccd2e"
   head "https://github.com/haensl/goto.git"
   depends_on "bash-completion"
+  bash_profile "#{ENV['HOME']}/.bash_profile"
 
   def install
     Pathname.new("#{prefix}/etc/bash_completion.d").install_symlink "goto.completion" "goto"
   end
 
   def post_install
-    system "bash", "touch #{ENV['HOME']}/.bash_profile"
-    system "bash", "\grep -q 'source #{prefix}/goto' #{ENV['HOME']}/.bash_profile || echo 'source #{prefix}/goto' >> #{ENV['HOME']}/.bash_profile"
+    system "bash", "touch #{bash_profile}"
+    system "bash", "\grep -q 'source #{prefix}/goto' #{bash_profile} || echo 'source #{prefix}/goto' >> #{bash_profile}"
   end
 end
