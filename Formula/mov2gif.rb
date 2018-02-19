669f20d4799f5d6b7f0a8b9642c3ca0c6ce70bc7
HP Dietz <hp@jandh.org>
2018-02-19 23:07:54 +0100

    Update mov2gif version.

--- Formula/mov2gif.rb
+++ Formula/mov2gif.rb
@@ -2,8 +2,8 @@ require 'pp'
 class Mov2Gif < Formula
   desc "Bash script to convert .mov videos to GIF animations."
   homepage "https://github.com/haensl/mov2gif"
-  url "https://github.com/haensl/mov2gif/archive/v1.0.0.tar.gz"
-  sha256 "42d5b016504c3b82d2d5e47681d2f64618f7419bb58e3f9dc0b7ea301278660c"
+  url "https://github.com/haensl/mov2gif/archive/v1.0.1.tar.gz"
+  sha256 "3dc5ca62f13f8d0571846e1d72d7ecf9acd23d87b7bc6f5f8c49ce05cd00bfd5"
   head "https://github.com/haensl/mov2gif.git"
   depends_on "ffmpeg"
   depends_on "imagemagick"
