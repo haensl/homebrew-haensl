80ba0cf4a1a9ee4ee1ba6710487de54bb940ef0d
HP Dietz <hp@jandh.org>
2019-01-14 12:26:01 +0100

    Update checksum.

--- Formula/openssl-generate-certificates.rb
+++ Formula/openssl-generate-certificates.rb
@@ -3,7 +3,7 @@ class OpensslGenerateCertificates < Formula
   desc "Generate self-signed certificates for client/server applications."
   homepage "https://github.com/haensl/openssl-certgen"
   url "https://github.com/haensl/openssl-certgen/archive/v1.2.2.tar.gz"
-  sha256 "69dc00aa7f2e3ddafbb16bad8cd49d3b0519bb469d88f379b63de4065a277b17"
+  sha256 "732ec4e0ad6dc5124d34893159dcaf32a90b7fcbaefc28c5baf6f1a65fb57616"
   head "https://github.com/haensl/openssl-certgen.git"
   depends_on "openssl"
 
