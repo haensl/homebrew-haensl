da0b83a56a98f9e78dadb3c241e6d4a92f65058a
HP Dietz <hp@jandh.org>
2019-11-15 16:05:28 +0100

    Update goto to v2.2.1

--- Formula/goto.rb
+++ Formula/goto.rb
@@ -2,8 +2,8 @@ require 'pp'
 class Goto < Formula
   desc "Bash completion for fast project navigation."
   homepage "https://github.com/haensl/goto"
-  url "https://github.com/haensl/goto/archive/v2.2.0.tar.gz"
-  sha256 "8d23f9ee132d77de80f36f9260453a8e169d3c5706f0ecf3f8b3337123bccd2e"
+  url "https://github.com/haensl/goto/archive/v2.2.1.tar.gz"
+  sha256 "893f3a38c1ef2d01724c0b11faf7495fc36409a606296af14bbe236756d87949"
   head "https://github.com/haensl/goto.git"
   depends_on "bash-completion"
 
